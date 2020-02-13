07980c8e32eb8e9be9bdcac3277d87ba93e5a47c
BipanaKarki <bipanakarki222@gmail.com>
2020-02-13 15:04:21 +0545

    Created Epasal and product description

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProductItem extends StatelessWidget {
@@ -8,7 +10,23 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(""),
+      child: CachedNetworkImage(
+        imageUrl: imgUrl,
+        fit: BoxFit.cover,
+        placeholder: (context, url) => CircularProgressIndicator(),
+        errorWidget: (context, url, error) => Icon(Icons.error),
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        title: Text(title, textAlign: TextAlign.center),
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+        ),
+      ),
     );
   }
 }
